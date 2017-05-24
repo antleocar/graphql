1edd1861478fee722f323a35d66f870999b84073
Shootr Network <shootr@MacBook-Pro-de-Shootr.local>
2017-05-24 12:47:25 +0200

    build dictionary

--- src/main/java/com/graphql/dao/schema/ObjectSchema.java
+++ src/main/java/com/graphql/dao/schema/ObjectSchema.java
@@ -1,12 +1,16 @@
 package com.graphql.dao.schema;
 import static graphql.schema.GraphQLSchema.newSchema;
 
+import com.graphql.dao.schema.types.CarConnection;
 import com.graphql.dao.schema.types.CarType;
+import com.graphql.dao.schema.types.UserConnection;
 import com.graphql.dao.schema.types.UserType;
+import com.graphql.dataFetcher.ConnectionDataFetcher;
 import com.graphql.dataFetcher.IdDataFetcher;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.GraphQLType;
+import java.util.HashSet;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,13 +30,27 @@ public class ObjectSchema {
   @Autowired
   private IdDataFetcher carIdDataFetcher;
 
+  @Autowired
+  private ConnectionDataFetcher userConnectionDataFetcher;
+
+  @Autowired
+  private ConnectionDataFetcher carConnectionDataFetcher;
+
 
   public GraphQLSchema build() {
     return newSchema().query(buildQuery()).build(buildDictionary());
   }
 
   private Set<GraphQLType> buildDictionary() {
-    return null;
+    Set<GraphQLType> types = new HashSet<>();
+
+    types.add(carType.buildCarType());
+    types.add(userType.buildUserType());
+
+    types.add(new UserConnection().buildUserConnection());
+    types.add(new CarConnection().buildCarConnection());
+
+    return types;
   }
 
   private GraphQLObjectType buildQuery() {
